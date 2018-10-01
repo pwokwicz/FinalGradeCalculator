88ebf5640359f3a1b9d09e2e7dfa8f5dc823112e
Parker Wokwicz <pwokwicz@parkers-macbook-air.local>
2018-10-01 11:35:22 -0500

    Initial Commit

---
+++ Final Grade Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Final Grade Calculator
+//
+//  Created by Parker Wokwicz on 10/1/18.
+//  Copyright © 2018 Parker Wokwicz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
